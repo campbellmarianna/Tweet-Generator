97f7a7b5d0b9dc518fbf2ed6a1a8d1d53dfca6da
Marianna <mckd2018@gmail.com>
2019-01-24 16:14:53 -0800

    add rearrange model

---
+++ rearrange.py
@@ -0,0 +1,21 @@
+# This code randomly rearranges a set of words provided as command-line arguments to the script.
+import random, sys
+
+# Capture the input from the terminal
+words = sys.argv[1:]
+
+def rearrange_words():
+    # Run a loop that runs for how many elements in the list
+    for w in range(0, len(words)):
+        # pair random index with list
+        rand_index = random.randint(0, len(words) -1)
+        word = words[w]
+        # print(word)
+        # remove element from list
+        word_taken_out = words.pop(rand_index)
+        # add element to the end of the list
+        words.append(word_taken_out)
+        # return list.join' '
+        return ' '.join(words)
+
+print(rearrange_words())
