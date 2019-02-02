c1d7aed6775048f79cb61499dd4e1930a70f46ff
Marianna <mckd2018@gmail.com>
2019-02-01 19:22:37 -0800

    add pseudocode for histogram in a list of tuples

--- histogram_tuple.py
+++ histogram_tuple.py
@@ -1,25 +1,19 @@
 source_text = "one fish two fish red fish blue fish"
 
 def histogram(source_text):
-    words_list = source_text.split(" ")
-    # create a list_of_tuples
-    list_of_tuples = []
-    # loop words_list
-    for word in words_list:
-        # loop through list_of_tuple / for tuple in list_of_tuple
-        found = False
-        for index, tuple in list_of_tuple:
-            #check if the first value in the current tuple is the word
-            if word is tuple[0]:
-                # set found to true
-                # store value2
-                freq = tuple[1] + 1
-                list_of_tuples.delete(tuple)
-                list_of_tuples.append((tuple[0], freq))
-                break
-                # if it is in there
-                # make the index[1] the number a variable
-                # remove that tuple
-        if not found:
-            list_of_tuples.append((word, 1))
-    return list_of_tuple
+    # create words_list
+    # create list_of_tuples = []
+    # loop through words_list
+        # found = False
+        # check if list of tuples is = 0
+            # append first word to list of tuples
+        # else
+            # loop (index, tuple) through enumerate list of tuples
+                # check if word is the same as tuple word
+                    # increment tuple[1] += 1
+                    # found is true
+                    # break
+                # check if not found
+                # append (word) to list of tuple
+
+print(histogram(source_text))
