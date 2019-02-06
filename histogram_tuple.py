98dbd12a8e60fc6a6a92ae776807ee7a22a71379
Marianna <mckd2018@gmail.com>
2019-02-05 18:05:18 -0800

    add histogram in tuple complete

--- histogram_tuple.py
+++ histogram_tuple.py
@@ -1,46 +1,35 @@
 source_text = "one fish two fish red fish blue fish"
 
-def histogram(source_text):
+def histogram(source_text): # Inspired by Jackson Ho
     # create words_list
-    words_list = source_text.split(" ")
-    # print(f"Words list: {words_list}")
-    # create list_of_tuples = []
+    words_list = source_text.split(' ')
+    # create list_of_tuple
     list_of_tuples = []
-    # print(list_of_tuples)
+    # append first [word, 1] to list_of_tuples
+    list_of_tuples.append((words_list[0], 0))
     # loop through words_list
     for word in words_list:
-        # found = False
+        # create flag found = False
         found = False
-        # check if list of tuples is = 0
-        if len(list_of_tuples) == 0:
-            # append first word to list of tuples
+        # loop through list of tuples
+        for index, value in enumerate(list_of_tuples):
+            # check if word is the same as tuple[0]
+            if value[0] == word:
+                #create var store freq + increment freq
+                num = value[1] + 1
+                # set flag to True
+                found = True
+                # take out that tuple
+                list_of_tuples.pop(index)
+                # append the word with the new freq
+                list_of_tuples.append((word, num))
+                # break
+                break
+        # if the word is not found
+        if not found:
+            # append the (word, 1) to the list of tuple
             list_of_tuples.append((word, 1))
-            # print(list_of_tuples)
-        # else
-        else:
-            # loop (index, tuple) through enumerate list of tuples
-            for index, tuple in enumerate(list_of_tuples):
-                # print(f"This should be a string: {tuple[0]}")
-                # check if word is the same as tuple word
-                if word == tuple[0]:
-                    # create var to store tuple int
-                    # increment var by 1
-                    freq = tuple[1] + 1
-                    # print(f"This should be a number {index}")
-                    # pop tuple out of list
-                    list_of_tuples.pop(index)
-                    # print(f"List before: {list_of_tuples}")
-                    # append tuple with word and new frequency
-                    list_of_tuples.append((word, freq))
-                    # print(f"List after: {list_of_tuples}")
-                    # found is true
-                    found = True
-                    # break
-                    break
-                # check if not found
-                if not found:
-                    # append (word) to list of tuple
-                    list_of_tuples.append((word, 1))
-    return(list_of_tuples)
+    # return list of tuple
+    return list_of_tuples
 
-print(histogram(source_text))
+histogram(source_text)
